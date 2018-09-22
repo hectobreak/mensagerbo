836ab36f06eecb04e0383ca628f88bff16ce7f67
hectobreak <hectobreack123@gmail.com>
2018-09-22 23:18:09 +0200

    testing

--- main.js
+++ main.js
@@ -1,2 +1,22 @@
+/*
+mensagerbo_credentials.js es un fichero de la siguiente forma:
+* var Discord = require('discord.js');
+* client = new Discord.Client();
+* client.login(TOKEN)
+* module.exports = client;
+*/
+var client = require("../mensagerbo_credentials.js");
 
 
+client.on('ready', () => {
+
+});
+client.on('message', async msg => {
+  /* msg.author.id       => Identificador de usuario.
+   * msg.author.username => Nombre de usuario.
+   * msg.content         => Contenido del mensaje.
+   * msg.channel         => Canal por el que se ha enviado el mensaje.
+   * Para más cosas, mirar la documentación de Discord.js.
+   */
+  msg.channel.send("Esto es una prueba.");
+});
